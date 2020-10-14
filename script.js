e127c6ed20d8f36ef571ed04b3e989099ffeba94
KobeHill68 <kobehill68@gmail.com>
2020-10-14 17:31:31 -0500

    added my make schedule function

--- script.js
+++ script.js
@@ -1,61 +1,12 @@
-$(document).ready(function(){
-    displayDate()
-    
-    
-    
-   function displayDate(){
-    var date= moment().format('LLLL');
-    $("#currentDay").text(date); 
-   }
 
-   var hour = 9;
-   var hourSuff = ':00am';
-   var lStore = [];
+var currtentday = moment().format("dddd MMMM Do YYYY");
+var currentH24 = moment().format("HH");
 
-   displayHourBlk(9);
+bussinessHour = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+schedule = [];
 
-   function displayHourBlk(WHoursInAday){
-    if(!WHoursInAday){
-       WHoursInAday = 1 
-    }
+init();
 
-    var time = moment().format('LT').toLowerCase().split('');
-    var currenthour = getCurrHour(time) + ':00'  + time.slice(-2)[0] + time.slice(-1)[0];
-
-
-
-   }
-
-
-   function getCurrHour(timeArray){
-    var i = 0;
-    var hour = '';
-
-    while(timeArray[i] !== ':' || i > 100){
-        hour += timeArray[i];
-        i++;
-    }
-    return hour;
-   };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
-
+function init(){
+    $("#currentDay").text(currtentday)
+}
